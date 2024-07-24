9690af067e9c2b4f1e38d3bf3ea6f3cfc56a6fa8
EWS777 <egor.vasilenko2004@gmail.com>
2024-07-24 23:42:26 +0200

    added exactly correct verification endpoints logic

---
+++ Wiedza.Api/Repositories/IVerificationRepository.cs
@@ -0,0 +1,13 @@
+﻿using Wiedza.Core.Models.Data;
+using Wiedza.Core.Utilities;
+
+namespace Wiedza.Api.Repositories;
+
+public interface IVerificationRepository
+{
+    Task<Verification[]> GetAllVerifications();
+    Task<Result<Verification>> GetVerificationAsync(Guid verificationId);
+    Task<Verification> AddVerificationAsync(Verification verification);
+    Task<Result<Verification>> UpdateVerificationStatusAsync(Guid verificationId, Action<Verification> update);
+    Task<Result<bool>> DeleteVerificationAsync(Guid verificationId);
+}
---
+++ Wiedza.Core/Services/IVerificationService.cs
@@ -0,0 +1,14 @@
+﻿using Wiedza.Core.Models.Data;
+using Wiedza.Core.Requests;
+using Wiedza.Core.Utilities;
+
+namespace Wiedza.Core.Services;
+
+public interface IVerificationService
+{
+    Task<Verification[]> GetAllVerificationsAsync();
+    Task<Result<Verification>> GetVerificationAsync(Guid verificationId, Guid userId);
+    Task<Verification> AddVerificationAsync(Guid userId, AddVerificationRequest request);
+    Task<Result<Verification>> UpdateVerificationStatusAsync(Guid verificationId, Guid userId, bool isCompleted);
+    Task<Result<bool>> DeleteVerificationAsync(Guid verificationId, Guid userId);
+}
